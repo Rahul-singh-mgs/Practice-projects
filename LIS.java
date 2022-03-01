f7eb7e239410bc8f21f92c33ff5d5e43535997e3
codewithrschauhan <95754597+codewithrschauhan@users.noreply.github.com>
2022-03-01 13:26:56 +0530

    Add files via upload

---
+++ LIS.java
@@ -0,0 +1,57 @@
+/* A Naive Java Program for LIS Implementation */
+class LIS {
+	static int max_ref; // stores the LIS
+
+	
+	static int _lis(int arr[], int n)
+	{
+		
+		if (n == 1)
+			return 1;
+
+		// 'max_ending_here' is length of LIS ending with
+		
+		// arr[n-1]
+		int res, max_ending_here = 1;
+
+		
+		for (int i = 1; i < n; i++) {
+			res = _lis(arr, i);
+			if (arr[i - 1] < arr[n - 1]
+				&& res + 1 > max_ending_here)
+				max_ending_here = res + 1;
+		}
+
+		// Compare max_ending_here with the overall max. And
+		// update the overall max if needed
+		
+		if (max_ref < max_ending_here)
+			max_ref = max_ending_here;
+
+		
+		return max_ending_here;
+	}
+
+	// The wrapper function for _lis()
+	static int lis(int arr[], int n)
+	{
+		// The max variable holds the result
+		max_ref = 1;
+
+		
+		_lis(arr, n);
+
+		// returns max
+		return max_ref;
+	}
+
+	
+	public static void main(String args[])
+	{
+		int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60, 70 ,80 };
+		int n = arr.length;
+		System.out.println("Length of lis is " + lis(arr, n)
+						+ "\n");
+	}
+}
+/*This code is contributed by Rajat Mishra*/
